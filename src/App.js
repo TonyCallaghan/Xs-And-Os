147befd407c80d14232279055fb00cfac75af83b
TonyCallaghan <A.tc.callaghan@gmail.com>
2021-08-20 16:39:46 +0100

    Calculate winner and update state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./components/Board";
+import { calcWinner } from "./helpers";
 import "./styles/root.scss";
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null)); // Board set as empty array,
+  const [nextPlayer, isXNext] = useState(false); // X or O
+
+  const winner = calcWinner(board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${nextPlayer ? 'X' : 'O'}`
+
+  const updateSquare = position => {
+    if (board[position] || winner) {                // if square already contains value,
+      return;                             // return from this function
+    }
+
+    setBoard((prev) => {
+      return prev.map((square, pos) => {  // if pos of currently iterated square is 
+        if (pos === position) {           // == position of currently clicked square
+          return nextPlayer ? 'X' : 'O';      // then return X if isXNext is true or O if false.
+        }
+
+        return square;
+      });
+    });
+
+    isXNext((prev => !prev));                // If isXNext is X(true), set to O(false)
+  };
+
   return (
     <div className="app">
       <h1>Xs & Os</h1>
-      <Board />
+      <h2>{message}</h2>
+      <Board board={board} updateSquare={updateSquare} />
     </div>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,31 +1,8 @@
-import React, { useState } from 'react' // {hook} for game state
+import React from 'react'                       // {hook} for game state
 import Square from './Square'
 
-const Board = () => {
-    const [board, setBoard] = useState(Array(9).fill(null)); // Board set as empty array,
-    const [isNext, xOrO] = useState(false); // X or O
-    console.log(board) // check array after onClick function
-    // 
-    const updateSquare = (position) => {
-        if (board[position]) { // if square already contains value,
-            return; // return from this function
-        }
-
-        // 
-        setBoard((prev) => {
-            return prev.map((square, pos) => {
-                // if pos of currently iterated square is 
-                // == position of currently clicked square
-                if (pos === position) {
-                    return isNext ? 'X' : 'O'; // then return X if xOrO is true or O if false.
-                }
-                return square;
-            });
-        });
-        xOrO((prev => !prev)) // If xOrO is X(true), set to O(false)
-    };
-
-    const renderSquare = (position) => { // Func to clean code below in board
+const Board = ({ board, updateSquare }) => {
+    const renderSquare = position => {        // Func to clean code below in board
         return (
             <Square
                 value={board[position]}
