6bc4712e42119c5cd8694af4688f031792881a37
TonyCallaghan <A.tc.callaghan@gmail.com>
2021-08-21 13:06:49 +0100

    State now includes game history

--- src/App.js
+++ src/App.js
@@ -4,38 +4,44 @@ import { calcWinner } from "./helpers";
 import "./styles/root.scss";
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null)); // Board set as empty array,
-  const [nextPlayer, isXNext] = useState(false); // X or O
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), nextPlayer: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+  console.log(history)
+  const current = history[currentMove];
 
-  const winner = calcWinner(board);
-
+  const winner = calcWinner(current.board);
   const message = winner
     ? `Winner is ${winner}`
-    : `Next player is ${nextPlayer ? 'X' : 'O'}`
+    : `Next player is ${current.nextPlayer ? 'X' : 'O'}`
 
   const updateSquare = position => {
-    if (board[position] || winner) {                // if square already contains value,
+    if (current.board[position] || winner) {                // if square already contains value,
       return;                             // return from this function
     }
 
-    setBoard((prev) => {
-      return prev.map((square, pos) => {  // if pos of currently iterated square is 
+    setHistory((prev) => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {  // if pos of currently iterated square is 
         if (pos === position) {           // == position of currently clicked square
-          return nextPlayer ? 'X' : 'O';      // then return X if isXNext is true or O if false.
+          return last.nextPlayer ? 'X' : 'O';      // then return X if isXNext is true or O if false.
         }
 
         return square;
       });
-    });
 
-    isXNext((prev => !prev));                // If isXNext is X(true), set to O(false)
+      return prev.concat({ board: newBoard, nextPlayer: !last.nextPlayer })
+    });
+    setCurrentMove(prev => prev + 1);
   };
 
   return (
     <div className="app">
       <h1>Xs & Os</h1>
       <h2>{message}</h2>
-      <Board board={board} updateSquare={updateSquare} />
+      <Board board={current.board} updateSquare={updateSquare} />
     </div>
   );
 };
